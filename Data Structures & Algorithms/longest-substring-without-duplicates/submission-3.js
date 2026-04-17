513443134d6b3a4f5b45b4a6b64220d8a7d62081
Ned <nedturner613@gmail.com>
2026-04-17 11:32:05 -0500

    Add: longest-substring-without-duplicates - submission-3

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-3.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+
+    lengthOfLongestSubstring(s) {
+        // let len = s.length;
+        // if(len<=1) return len;
+        // let set = new Set();
+        // let l = 0;
+        // let best = 1;
+        // for(let r =0;r<len;r++){
+        //     console.log(`R = ${r}`)
+        //     let char = s[r];
+        //     while(set.has(char)){
+        //         console.log(`   ${s.substring(l,r+1)}`);
+        //         set.delete(s[l]);
+        //         l++;
+        //     }
+        //     set.add(char);
+        //     console.log(`   ${s.substring(l,r+1)}`);
+        //     best = Math.max(best,r-l+1);
+        // }
+        // return best;
+        let mp = new Map();
+        let l =0;
+        let res = 0;
+        for(let r =0;r<s.length;r++){
+            if(mp.has(s[r])) l = Math.max(mp.get(s[r])+1,l);
+            mp.set(s[r], r); 
+            res = Math.max(res,r-l+1);
+        }
+        return res;
+    }
+}
