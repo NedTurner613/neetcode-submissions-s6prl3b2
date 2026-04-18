61bca6b7f84ea6506eba0b6d1a3cc92541cfa160
Ned <nedturner613@gmail.com>
2026-04-18 08:46:52 -0500

    Add: merge-intervals - submission-2

---
+++ Data Structures & Algorithms/merge-intervals/submission-2.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number[][]}
+     */
+    merge(intervals) {
+        intervals.sort((a,b)=>a[0]-b[0]);
+        let merged = new Map();
+        merged.set(intervals[0][0],intervals[0][1]);
+        let l =intervals[0][0];
+
+        for(let r=1; r< intervals.length;r++){
+            let intL=intervals[r][0];
+            let intR=intervals[r][1];
+            if(intL<=merged.get(l)) merged.set(l,Math.max(merged.get(l),intR));
+            else{
+                merged.set(intL,intR);
+                l = intL;
+            } 
+        }
+        console.log(merged)
+        
+        for (const [key, value] of merged) {
+        console.log(`${key}: ${value}`);
+        }
+        // return Array.from(merged,(key,value)=>([key,value]));
+        return [...merged];
+
+    }
+}
